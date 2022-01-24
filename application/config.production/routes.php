ba06a8cf2a433126e235dfdfdbc9effdb16a7786
maspriyambodo <maspriyambodo@gmail.com>
2022-01-24 20:24:38 +0700

    add route to report brand

--- application/config.production/routes.php
+++ application/config.production/routes.php
@@ -2,6 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Auth';
+$route['Laporan1%(:any)'] = 'Report/Brand/Bulan/index/';
 $route['Change%(:any)'] = 'Systems/Change/';
 $route['Setting%20Profile'] = 'Systems/Profile/';
 $route['Dashboard'] = 'Applications/Dashboard/index/';
