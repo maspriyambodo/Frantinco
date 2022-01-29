17339b4c1abcd8d4cd62b867fa410486b917f420
maspriyambodo <maspriyambodo@gmail.com>
2022-01-29 22:32:19 +0700

    add route report category

--- application/config.production/routes.php
+++ application/config.production/routes.php
@@ -3,6 +3,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Auth';
 $route['Laporan1%(:any)'] = 'Report/Brand/Bulan/index/';
+$route['Laporan2%(:any)'] = 'Report/Category/Bulan/index/';
 $route['Change%(:any)'] = 'Systems/Change/';
 $route['Setting%20Profile'] = 'Systems/Profile/';
 $route['Dashboard'] = 'Applications/Dashboard/index/';
