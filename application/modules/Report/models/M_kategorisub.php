b38039ae89ea4bb40b248ac9903600b26c1baa02
maspriyambodo <maspriyambodo@gmail.com>
2022-01-29 22:00:27 +0700

    add module report category sub: 	new file:   application/modules/Report/controllers/Categorysub/Dashboard.php 	new file:   application/modules/Report/models/M_kategorisub.php 	new file:   application/modules/Report/views/kategorisub/kategori_index.php

---
+++ application/modules/Report/models/M_kategorisub.php
@@ -0,0 +1,65 @@
+<?php
+
+defined('BASEPATH') OR exit('trying to signin backdoor?');
+
+class M_kategorisub extends CI_Model {
+
+    public function Dir_year() {
+        $exec = $this->db->select()
+                ->from('report_get_year')
+                ->get()
+                ->result();
+        return $exec;
+    }
+
+    public function dt_table($tahun) {
+        $exec = $this->db->select('v_transaction.id_categorysub,v_transaction.nama_kategorisub,
+	MIN(v_transaction.tr_date) AS tr_date,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 01 ) THEN v_transaction.qty ELSE 0 END ) AS `JANUARI`,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 02 ) THEN v_transaction.qty ELSE 0 END ) AS `FEBRUARI`,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 03 ) THEN v_transaction.qty ELSE 0 END ) AS `MARET`,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 04 ) THEN v_transaction.qty ELSE 0 END ) AS `APRIL`,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 05 ) THEN v_transaction.qty ELSE 0 END ) AS `MEI`,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 06 ) THEN v_transaction.qty ELSE 0 END ) AS `JUNI`,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 07 ) THEN v_transaction.qty ELSE 0 END ) AS `JULI`,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 08 ) THEN v_transaction.qty ELSE 0 END ) AS `AGUSTUS`,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 09 ) THEN v_transaction.qty ELSE 0 END ) AS `SEPTEMBER`,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 10 ) THEN v_transaction.qty ELSE 0 END ) AS `OKTOBER`,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 11 ) THEN v_transaction.qty ELSE 0 END ) AS `NOVEMBER`,
+	sum( CASE WHEN ( MONTH ( v_transaction.tr_date ) = 12 ) THEN v_transaction.qty ELSE 0 END ) AS `DESEMBER`')
+                ->from('mt_category_sub')
+                ->join('v_transaction', 'mt_category_sub.id = v_transaction.id_categorysub', 'LEFT')
+                ->where('`mt_category_sub`.`stat`', 1, false)
+                ->where('YEAR ( `v_transaction`.`tr_date` ) = ', $tahun, false)
+                ->group_by('mt_category_sub.id')
+                ->get()
+                ->result();
+        return $exec;
+    }
+
+    public function chartdiv($tahun) {
+        $exec = $this->db->select('mt_month.bulan,
+	SUM( CASE WHEN v_transaction.qty > 0 THEN v_transaction.qty ELSE 0 END ) AS qty')
+                ->from('mt_month')
+                ->join('v_transaction', 'mt_month.angka = MONTH ( v_transaction.tr_date )', 'LEFT')
+                ->where('EXISTS( SELECT MONTH ( v_transaction.tr_date) FROM v_transaction WHERE YEAR ( v_transaction.tr_date ) = ' . $tahun . ' AND mt_month.angka = MONTH ( v_transaction.tr_date ) )')
+                ->or_where('NOT EXISTS( SELECT MONTH ( v_transaction.tr_date) FROM v_transaction WHERE YEAR ( v_transaction.tr_date ) = ' . $tahun . ' AND mt_month.angka = MONTH ( v_transaction.tr_date ) )', null, false)
+                ->group_by('mt_month.angka')
+                ->get()
+                ->result();
+        return $exec;
+    }
+
+    public function chartdiv_a($tahun) {
+        $exec = $this->db->select('v_transaction.nama_kategorisub,SUM( v_transaction.qty ) AS qty,v_transaction.kode_product')
+                ->from('mt_category_sub')
+                ->join('v_transaction', 'mt_category_sub.id = v_transaction.id_categorysub')
+                ->where('YEAR ( v_transaction.tr_date ) =', $tahun, false)
+                ->group_by('mt_category_sub.id')
+                ->limit(30)
+                ->get()
+                ->result();
+        return $exec;
+    }
+
+}
---
+++ application/modules/Report/views/kategorisub/kategori_index.php
@@ -0,0 +1,303 @@
+<script src="https://cdn.amcharts.com/lib/4/core.js"></script>
+<script src="https://cdn.amcharts.com/lib/4/charts.js"></script>
+<script src="https://cdn.amcharts.com/lib/4/themes/animated.js"></script>
+<div class="card card-custom">
+    <div class="card-body">
+        <div class="form-group row">
+            <label for="tahuntxt" class="col-md-2 col-form-label">Choose Year:</label>
+            <div class="col-md-4">
+                <select id="tahuntxt" name="tahuntxt" class="form-control custom-select" onchange="Tahun(this.value)">
+                    <?php
+                    foreach ($year as $value) {
+                        if ($value->text == date('Y')) {
+                            echo '<option value="' . $value->id . '" selected>' . $value->text . '</option>';
+                        } else {
+                            echo '<option value="' . $value->id . '">' . $value->text . '</option>';
+                        }
+                    }
+                    ?>
+                </select>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="clearfix my-4"></div>
+<div class="card card-custom" data-card="true">
+    <div class="card-header">
+        <div class="card-title">
+            Annual Report
+        </div>
+        <div class="card-toolbar">
+            <a href="#" class="btn btn-icon btn-sm btn-hover-light-primary mr-1" data-card-tool="toggle" data-toggle="tooltip" data-placement="top" title="minimize">
+                <i class="ki ki-arrow-down icon-nm"></i>
+            </a>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="text-center">
+            <b><u id="title_chartdiv" class="count"></u></b>
+        </div>
+        <div id="chartdiv" class="chartdivs"></div>
+    </div>
+</div>
+<div class="clearfix my-4"></div>
+<div class="card card-custom" data-card="true">
+    <div class="card-header">
+        <div class="card-title">
+            Report by Sub Category
+        </div>
+        <div class="card-toolbar">
+            <a href="#" class="btn btn-icon btn-sm btn-hover-light-primary mr-1" data-card-tool="toggle" data-toggle="tooltip" data-placement="top" title="minimize">
+                <i class="ki ki-arrow-down icon-nm"></i>
+            </a>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="text-center">
+            <b><u id="title_chartdiv_a" class="count"></u></b>
+        </div>
+        <div id="chartdiv_a" class="chartdivs"></div>
+    </div>
+</div>
+<div class="clearfix my-4"></div>
+<div class="card card-custom" data-card="true">
+    <div class="card-header">
+        <div class="card-title">
+            Report details
+        </div>
+        <div class="card-toolbar">
+            <a href="#" class="btn btn-icon btn-sm btn-hover-light-primary mr-1" data-card-tool="toggle" data-toggle="tooltip" data-placement="top" title="minimize">
+                <i class="ki ki-arrow-down icon-nm"></i>
+            </a>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover table-striped" style="width:100%;"></table>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function () {
+
+        var tahun = $('select[name="tahuntxt"]').val();
+        chartdiv(tahun);
+        chartdiv_a(tahun);
+        dt_tabel(tahun);
+
+        function Tahun(id) {
+            var tahuntxt = $("#tahuntxt option:selected").text();
+            document.getElementById('pagetitle').innerHTML = 'Category Report ' + tahuntxt;
+            chartdiv(id);
+            chartdiv_a(id);
+            $('table').DataTable().clear();
+            $('table').DataTable().destroy();
+            dt_tabel(id);
+        }
+
+        function chartdiv(year) {
+            am4core.ready(function () {
+                am4core.useTheme(am4themes_animated);
+                am4core.addLicense("ch-custom-attribution");
+
+                var chart = am4core.create("chartdiv", am4charts.XYChart);
+                chart.colors.step = 2;
+                chart.scrollbarX = new am4core.Scrollbar();
+                chart.exporting.menu = new am4core.ExportMenu();
+                chart.dataSource.url = 'Report/Categorysub/Dashboard/chartdiv?token=' + year;
+
+                var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+                categoryAxis.dataFields.category = "bulan";
+                categoryAxis.renderer.grid.template.location = 0;
+                categoryAxis.renderer.minGridDistance = 12;
+                categoryAxis.renderer.labels.template.horizontalCenter = "right";
+                categoryAxis.renderer.labels.template.verticalCenter = "middle";
+                categoryAxis.renderer.labels.template.rotation = 300;
+                categoryAxis.tooltip.disabled = true;
+                categoryAxis.renderer.minHeight = 30;
+                categoryAxis.title.text = 'Month of Sales';
+                categoryAxis.title.fontWeight = 800;
+
+                let label = categoryAxis.renderer.labels.template;
+                label.wrap = true;
+                label.maxWidth = 120;
+
+                var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+                valueAxis.renderer.minWidth = 50;
+                valueAxis.title.text = "Total Sales";
+                valueAxis.title.fontWeight = 800;
+
+                var series = chart.series.push(new am4charts.ColumnSeries());
+                series.sequencedInterpolation = true;
+                series.dataFields.valueY = "qty";
+                series.dataFields.categoryX = "bulan";
+                series.tooltipText = "Total Sales month {categoryX}: [bold]{valueY}[/]";
+                series.columns.template.strokeWidth = 0;
+                series.tooltip.pointerOrientation = "vertical";
+                series.columns.template.column.cornerRadiusTopLeft = 10;
+                series.columns.template.column.cornerRadiusTopRight = 10;
+                series.columns.template.column.fillOpacity = 0.8;
+
+                var hoverState = series.columns.template.column.states.create("hover");
+                hoverState.properties.cornerRadiusTopLeft = 0;
+                hoverState.properties.cornerRadiusTopRight = 0;
+                hoverState.properties.fillOpacity = 1;
+
+                chart.cursor = new am4charts.XYCursor();
+            });
+        }
+        
+        function chartdiv_a(year) {
+            am4core.ready(function () {
+                am4core.useTheme(am4themes_animated);
+                am4core.addLicense("ch-custom-attribution");
+                
+                var chart = am4core.create("chartdiv_a", am4charts.XYChart);
+                chart.colors.step = 2;
+                chart.scrollbarX = new am4core.Scrollbar();
+                chart.exporting.menu = new am4core.ExportMenu();
+                chart.dataSource.url = 'Report/Categorysub/Dashboard/chartdiv_a?token=' + year;
+                
+                var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+                categoryAxis.dataFields.category = "kode_product";
+                categoryAxis.renderer.grid.template.location = 0;
+                categoryAxis.renderer.minGridDistance = 12;
+                categoryAxis.renderer.labels.template.horizontalCenter = "right";
+                categoryAxis.renderer.labels.template.verticalCenter = "middle";
+                categoryAxis.renderer.labels.template.rotation = 300;
+                categoryAxis.tooltip.disabled = true;
+                categoryAxis.renderer.minHeight = 30;
+                categoryAxis.title.text = 'Product Line';
+                categoryAxis.title.fontWeight = 800;
+                
+                let label = categoryAxis.renderer.labels.template;
+                label.wrap = true;
+                label.maxWidth = 120;
+                
+                var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+                valueAxis.renderer.minWidth = 50;
+                valueAxis.title.text = "Total Sales";
+                valueAxis.title.fontWeight = 800;
+
+                var series = chart.series.push(new am4charts.ColumnSeries());
+                series.sequencedInterpolation = true;
+                series.dataFields.valueY = "qty";
+                series.dataFields.categoryX = "kode_product";
+                series.tooltipText = "Total Sales {nama_kategorisub}: [bold]{valueY}[/]";
+                series.columns.template.strokeWidth = 0;
+                series.tooltip.pointerOrientation = "vertical";
+                series.columns.template.column.cornerRadiusTopLeft = 10;
+                series.columns.template.column.cornerRadiusTopRight = 10;
+                series.columns.template.column.fillOpacity = 0.8;
+
+                var hoverState = series.columns.template.column.states.create("hover");
+                hoverState.properties.cornerRadiusTopLeft = 0;
+                hoverState.properties.cornerRadiusTopRight = 0;
+                hoverState.properties.fillOpacity = 1;
+                
+                chart.cursor = new am4charts.XYCursor();
+            });
+        }
+        
+        function dt_tabel(year) {
+            $('table').dataTable({
+                "serverSide": false,
+                "order": [[0, "asc"]],
+                "paging": true,
+                "ordering": true,
+                "info": true,
+                "processing": true,
+                "deferRender": true,
+                "scrollCollapse": true,
+                "scrollX": true,
+                "scrollY": "400px",
+                dom: `<'row'<'col-sm-6 text-left'l><'col-sm-6 text-right'f>>
+                <'row'<'col-sm-12'tr>>
+                <'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'p>>`,
+                "ajax": {
+                    "url": "Report/Categorysub/Dashboard/dt_table?token=" + year,
+                    dataSrc: ''
+                },
+                columns: [
+                    {
+                        data: 'nama_kategorisub',
+                        title: 'Category'
+                    },
+                    {
+                        data: 'JANUARI',
+                        title: 'JAN',
+                        className: "text-center",
+                        "searchable": false
+                    },
+                    {
+                        data: 'FEBRUARI',
+                        title: 'FEB',
+                        className: "text-center",
+                        "searchable": false
+                    },
+                    {
+                        data: 'MARET',
+                        title: 'MAR',
+                        className: "text-center",
+                        "searchable": false
+                    },
+                    {
+                        data: 'APRIL',
+                        title: 'APR',
+                        className: "text-center",
+                        "searchable": false
+                    },
+                    {
+                        data: 'MEI',
+                        title: 'MEI',
+                        className: "text-center",
+                        "searchable": false
+                    },
+                    {
+                        data: 'JUNI',
+                        title: 'JUN',
+                        className: "text-center",
+                        "searchable": false
+                    },
+                    {
+                        data: 'JULI',
+                        title: 'JUL',
+                        className: "text-center",
+                        "searchable": false
+                    },
+                    {
+                        data: 'AGUSTUS',
+                        title: 'AUG',
+                        className: "text-center",
+                        "searchable": false
+                    },
+                    {
+                        data: 'SEPTEMBER',
+                        title: 'SEP',
+                        className: "text-center",
+                        "searchable": false
+                    },
+                    {
+                        data: 'OKTOBER',
+                        title: 'OCT',
+                        className: "text-center",
+                        "searchable": false
+                    },
+                    {
+                        data: 'NOVEMBER',
+                        title: 'NOV',
+                        className: "text-center",
+                        "searchable": false
+                    },
+                    {
+                        data: 'DESEMBER',
+                        title: 'DEC',
+                        className: "text-center",
+                        "searchable": false
+                    }
+                ]
+            });
+        }
+
+    });
+</script>
