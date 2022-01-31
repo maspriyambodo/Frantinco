ee9ea12466e67b044a20e499889ce2f0008ff7e1
maspriyambodo <maspriyambodo@gmail.com>
2022-01-31 21:06:26 +0700

    change style chart

--- application/modules/Report/views/brand/report_brand.js
+++ application/modules/Report/views/brand/report_brand.js
@@ -27,56 +27,59 @@ function Chart_1(year) {
     });
 }
 function chartdiv(year) {
-        am4core.ready(function () {
-            am4core.useTheme(am4themes_animated);
-            am4core.addLicense("ch-custom-attribution");
+    am4core.ready(function () {
+        am4core.useTheme(am4themes_animated);
+        am4core.addLicense("ch-custom-attribution");
 
-            var chart = am4core.create("chartdiv", am4charts.XYChart);
-            chart.colors.step = 2;
-            chart.scrollbarX = new am4core.Scrollbar();
-            chart.exporting.menu = new am4core.ExportMenu();
-            chart.dataSource.url = 'Report/Category/Dashboard/chartdiv?token=' + year;
+        var chart = am4core.create("chartdiv", am4charts.XYChart);
+        chart.colors.step = 2;
+        chart.scrollbarX = new am4core.Scrollbar();
+        chart.exporting.menu = new am4core.ExportMenu();
+        chart.dataSource.url = 'Report/Category/Dashboard/chartdiv?token=' + year;
 
-            var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-            categoryAxis.dataFields.category = "bulan";
-            categoryAxis.renderer.grid.template.location = 0;
-            categoryAxis.renderer.minGridDistance = 12;
-            categoryAxis.renderer.labels.template.horizontalCenter = "right";
-            categoryAxis.renderer.labels.template.verticalCenter = "middle";
-            categoryAxis.renderer.labels.template.rotation = 300;
-            categoryAxis.tooltip.disabled = true;
-            categoryAxis.renderer.minHeight = 30;
-            categoryAxis.title.text = 'Month of Sales';
-            categoryAxis.title.fontWeight = 800;
+        var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "bulan";
+        categoryAxis.renderer.grid.template.location = 0;
+        categoryAxis.renderer.minGridDistance = 12;
+        categoryAxis.renderer.labels.template.horizontalCenter = "right";
+        categoryAxis.renderer.labels.template.verticalCenter = "middle";
+        categoryAxis.renderer.labels.template.rotation = 300;
+        categoryAxis.tooltip.disabled = true;
+        categoryAxis.renderer.minHeight = 30;
+        categoryAxis.title.text = 'Month of Sales';
+        categoryAxis.title.fontWeight = 800;
 
-            let label = categoryAxis.renderer.labels.template;
-            label.wrap = true;
-            label.maxWidth = 120;
+        let label = categoryAxis.renderer.labels.template;
+        label.wrap = true;
+        label.maxWidth = 120;
 
-            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-            valueAxis.renderer.minWidth = 50;
-            valueAxis.title.text = "Total Sales";
-            valueAxis.title.fontWeight = 800;
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.renderer.minWidth = 50;
+        valueAxis.title.text = "Total Sales";
+        valueAxis.title.fontWeight = 800;
 
-            var series = chart.series.push(new am4charts.ColumnSeries());
-            series.sequencedInterpolation = true;
-            series.dataFields.valueY = "qty";
-            series.dataFields.categoryX = "bulan";
-            series.tooltipText = "Total Sales month {categoryX}: [bold]{valueY}[/]";
-            series.columns.template.strokeWidth = 0;
-            series.tooltip.pointerOrientation = "vertical";
-            series.columns.template.column.cornerRadiusTopLeft = 10;
-            series.columns.template.column.cornerRadiusTopRight = 10;
-            series.columns.template.column.fillOpacity = 0.8;
-
-            var hoverState = series.columns.template.column.states.create("hover");
-            hoverState.properties.cornerRadiusTopLeft = 0;
-            hoverState.properties.cornerRadiusTopRight = 0;
-            hoverState.properties.fillOpacity = 1;
-
-            chart.cursor = new am4charts.XYCursor();
+        var series = chart.series.push(new am4charts.ColumnSeries());
+        series.sequencedInterpolation = true;
+        series.dataFields.valueY = "qty";
+        series.dataFields.categoryX = "bulan";
+        series.tooltipText = "Total Sales month {categoryX}: [bold]{valueY}[/]";
+        series.columns.template.strokeWidth = 0;
+        series.tooltip.pointerOrientation = "vertical";
+        series.columns.template.column.cornerRadiusTopLeft = 10;
+        series.columns.template.column.cornerRadiusTopRight = 10;
+        series.columns.template.column.fillOpacity = 0.8;
+        series.columns.template.adapter.add("fill", function (fill, target) {
+            return chart.colors.getIndex(target.dataItem.index);
         });
-    }
+
+        var hoverState = series.columns.template.column.states.create("hover");
+        hoverState.properties.cornerRadiusTopLeft = 0;
+        hoverState.properties.cornerRadiusTopRight = 0;
+        hoverState.properties.fillOpacity = 1;
+
+        chart.cursor = new am4charts.XYCursor();
+    });
+}
 function dt_tabel(year) {
     $('table').dataTable({
         "serverSide": false,
