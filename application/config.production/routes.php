07d0a42cae9c50da6f3d3579ccf3923aee66cc39
maspriyambodo <maspriyambodo@gmail.com>
2022-02-01 22:45:27 +0700

    add routes to report subcategory

--- application/config.production/routes.php
+++ application/config.production/routes.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Auth';
 $route['Laporan1%(:any)'] = 'Report/Brand/Bulan/index/';
 $route['Laporan2%(:any)'] = 'Report/Category/Bulan/index/';
+$route['Laporan3%(:any)'] = 'Report/Categorysub/Bulan/index/';
 $route['Change%(:any)'] = 'Systems/Change/';
 $route['Setting%20Profile'] = 'Systems/Profile/';
 $route['Dashboard'] = 'Applications/Dashboard/index/';
