f4662a1b496bcdad04b1909a3d59803cd40396a3
maspriyambodo <maspriyambodo@gmail.com>
2022-02-01 22:41:15 +0700

    Changes to be committed: 	new file:   application/modules/Report/controllers/Categorysub/Bulan.php 	modified:   application/modules/Report/controllers/Categorysub/Dashboard.php 	new file:   application/modules/Report/models/M_kategorisub_bulan.php 	new file:   application/modules/Report/views/kategorisub/month_index.php

--- application/modules/Report/controllers/Categorysub/Dashboard.php
+++ application/modules/Report/controllers/Categorysub/Dashboard.php
@@ -61,7 +61,35 @@ class Dashboard extends CI_Controller {
                 $result = $dt_table;
             }
         }
-        return ToJson($result);
+        return $this->_dttables($result);
+    }
+
+    private function _dttables($param) {
+        if (!empty($param)) {
+            foreach ($param as $value) {
+                $tgl = date_create($value->tr_date);
+                $data[] = [
+                    'id_categorysub' => $value->id_categorysub,
+                    'nama_kategorisub' => $value->nama_kategorisub,
+                    'tr_date' => $value->tr_date,
+                    'JANUARI' => ($value->JANUARI == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=01' . '&c=' . $value->id_categorysub . '&d=' . $value->JANUARI)) . '">' . number_format($value->JANUARI) . '</a>',
+                    'FEBRUARI' => ($value->FEBRUARI == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=02' . '&c=' . $value->id_categorysub . '&d=' . $value->FEBRUARI)) . '">' . number_format($value->FEBRUARI) . '</a>',
+                    'MARET' => ($value->MARET == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=03' . '&c=' . $value->id_categorysub . '&d=' . $value->MARET)) . '">' . number_format($value->MARET) . '</a>',
+                    'APRIL' => ($value->APRIL == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=04' . '&c=' . $value->id_categorysub . '&d=' . $value->APRIL)) . '">' . number_format($value->APRIL) . '</a>',
+                    'MEI' => ($value->MEI == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=05' . '&c=' . $value->id_categorysub . '&d=' . $value->MEI)) . '">' . number_format($value->MEI) . '</a>',
+                    'JUNI' => ($value->JUNI == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=06' . '&c=' . $value->id_categorysub . '&d=' . $value->JUNI)) . '">' . number_format($value->JUNI) . '</a>',
+                    'JULI' => ($value->JULI == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=07' . '&c=' . $value->id_categorysub . '&d=' . $value->JULI)) . '">' . number_format($value->JULI) . '</a>',
+                    'AGUSTUS' => ($value->AGUSTUS == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=08' . '&c=' . $value->id_categorysub . '&d=' . $value->AGUSTUS)) . '">' . number_format($value->AGUSTUS) . '</a>',
+                    'SEPTEMBER' => ($value->SEPTEMBER == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=09' . '&c=' . $value->id_categorysub . '&d=' . $value->SEPTEMBER)) . '">' . number_format($value->SEPTEMBER) . '</a>',
+                    'OKTOBER' => ($value->OKTOBER == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=10' . '&c=' . $value->id_categorysub . '&d=' . $value->OKTOBER)) . '">' . number_format($value->OKTOBER) . '</a>',
+                    'NOVEMBER' => ($value->NOVEMBER == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=11' . '&c=' . $value->id_categorysub . '&d=' . $value->NOVEMBER)) . '">' . number_format($value->NOVEMBER) . '</a>',
+                    'DESEMBER' => ($value->DESEMBER == 0) ? '<span class="text-danger">0</span>' : '<a href="' . base_url('Laporan3%23?token=' . Enkrip('?a=' . date_format($tgl, 'Y') . '&b=12' . '&c=' . $value->id_categorysub . '&d=' . $value->DESEMBER)) . '">' . number_format($value->DESEMBER) . '</a>'
+                ];
+            }
+        } else {
+            $data = [];
+        }
+        return ToJson($data);
     }
 
     public function chartdiv() {
---
+++ application/modules/Report/models/M_kategorisub_bulan.php
@@ -0,0 +1,62 @@
+<?php
+
+defined('BASEPATH') OR exit('trying to signin backdoor?');
+
+class M_kategorisub_bulan extends CI_Model {
+
+    var $table = 'v_transaction';
+    var $column_order = ['v_transaction.id_categorysub', 'v_transaction.nama_kategori', 'v_transaction.nama_kategorisub', 'v_transaction.nama_produk', 'v_transaction.kode_product', 'v_transaction.tr_date', 'v_transaction.qty']; //set column field database for datatable orderable
+    var $column_search = ['v_transaction.qty', 'v_transaction.tr_date', 'v_transaction.kode_product', 'v_transaction.nama_kategori', 'v_transaction.nama_kategorisub', 'v_transaction.nama_produk']; //set column field database for datatable searchable 
+    var $order = ['v_transaction.id_categorysub' => 'ASC']; // default order
+
+    private function _get_datatables_query($param) {
+        $this->db->select('v_transaction.id_categorysub,v_transaction.qty,v_transaction.tr_date,v_transaction.kode_product AS kode,v_transaction.nama_kategori AS nama_category,v_transaction.nama_kategorisub AS sub_category,v_transaction.nama_produk AS nama_product')
+                ->from('v_transaction')
+                ->where('YEAR ( v_transaction.tr_date ) =', $param[0], false)
+                ->where('MONTH ( v_transaction.tr_date ) =', $param[1], false)
+                ->where('`v_transaction`.`id_categorysub`', $param[2], false);
+        $i = 0;
+        foreach ($this->column_search as $item) { // loop column 
+            if ($_GET['search']['value']) { // if datatable send POST for search
+                if ($i === 0) { // first loop
+                    $this->db->group_start(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
+                    $this->db->like($item, $_GET['search']['value']);
+                } else {
+                    $this->db->or_like($item, $_GET['search']['value']);
+                }
+
+                if (count($this->column_search) - 1 == $i) //last loop
+                    $this->db->group_end(); //close bracket
+            }
+            $i++;
+        }
+
+        if (isset($_GET['order'])) { // here order processing
+            $this->db->order_by($this->column_order[$_GET['order']['0']['column']], $_GET['order']['0']['dir']);
+        } else if (isset($this->order)) {
+            $order = $this->order;
+            $this->db->order_by(key($order), $order[key($order)]);
+        }
+    }
+
+    public function lists($param) {
+        $this->_get_datatables_query($param);
+        if ($_GET['length'] != -1)
+            $this->db->limit($_GET['length'], $_GET['start']);
+        $query = $this->db->get();
+        log_message('error', $this->db->last_query());
+        return $query->result();
+    }
+
+    public function count_filtered($param) {
+        $this->_get_datatables_query($param);
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+
+    public function count_all($param) {
+        $this->_get_datatables_query($param);
+        return $this->db->count_all_results();
+    }
+
+}
