ff349957186f5e1fcfe07fef06ffad08e6998e02
maspriyambodo <maspriyambodo@gmail.com>
2022-02-16 14:36:33 +0700

    add route to report product

--- application/config.production/routes.php
+++ application/config.production/routes.php
@@ -5,6 +5,7 @@ $route['default_controller'] = 'Auth';
 $route['Laporan1%(:any)'] = 'Report/Brand/Bulan/index/';
 $route['Laporan2%(:any)'] = 'Report/Category/Bulan/index/';
 $route['Laporan3%(:any)'] = 'Report/Categorysub/Bulan/index/';
+$route['Laporan4%(:any)'] = 'Report/Category/Product_report/index/';
 $route['Change%(:any)'] = 'Systems/Change/';
 $route['Setting%20Profile'] = 'Systems/Profile/';
 $route['Dashboard'] = 'Applications/Dashboard/index/';
